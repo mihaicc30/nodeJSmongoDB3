6df96e62e01b4795247e1b52a6b8730dfebd0e8a
mihaicc30 <77832895+mihaicc30@users.noreply.github.com>
2022-03-07 17:00:38 +0000

    twks5

--- config/script.js
+++ config/script.js
@@ -2,15 +2,18 @@
 mybutton = document.getElementById("toTheTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction();};
+window.onscroll = function() {scrollFunction(),rotateLogo();};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 240 || document.documentElement.scrollTop > 240) {
-    document.getElementById("toTheTopBtn").style.display = "block";
-  } else {
-    document.getElementById("toTheTopBtn").style.display = "none";
+  if (window.location.pathname == "/findaroom") {
+    if (document.body.scrollTop > 240 || document.documentElement.scrollTop > 240) {
+      document.getElementById("toTheTopBtn").style.display = "block";
+    } else {
+      document.getElementById("toTheTopBtn").style.display = "none";
+    }
   }
 }
+  
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -22,3 +25,24 @@ function scrollzTo(choice) {
     document.getElementById("choice"+choice+"").scrollIntoView({behavior: 'smooth', block: 'center', inline: 'nearest'});
 }
 
+let width = screen.width;
+const logo = document.getElementById('myLogo2');
+
+function rotateLogo() {
+  var scrollPos = window.scrollY;
+  if(window.scrollY == 0) {
+    logo.style.position="initial";
+  } 
+  if(window.scrollY < 200) {
+    logo.style.position="initial";
+  } 
+
+  if(scrollPos > 200 && width > 700) {
+  logo.style.position="fixed";
+  logo.style.zIndex="99";
+  var scrollPos = window.scrollY;
+  logo.style.top="50px";
+  logo.style.left="30px";
+  logo.style.transform = "rotate(" + scrollPos + "deg)";
+  }
+};
