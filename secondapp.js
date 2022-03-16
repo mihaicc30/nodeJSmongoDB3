48ae195f2a8df17b7faa9673a1063f6466fee72d
mihaicc30 <77832895+mihaicc30@users.noreply.github.com>
2022-03-16 13:58:24 +0000

    rework footer

--- secondapp.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) =>
-    res.render('../index.ejs')
-)
-
-app.listen(5555, console.log(`Server running on 5555`));
