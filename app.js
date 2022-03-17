8721a2ca5a5b4ff968c4fb6f6637b217509ef62d
mihaicc30 <77832895+mihaicc30@users.noreply.github.com>
2022-03-17 21:11:47 +0000

    fixed memory leak? maybe

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
-const session = require('express-session');
+const session = require('cookie-session');
 const app = express();
 
 // Passport Config
